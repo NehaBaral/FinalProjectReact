b18bbfd4e376f37b7270d419d1939fe73bdacf15
NehaBaral <41846320+NehaBaral@users.noreply.github.com>
2024-11-21 23:17:58 -0500

    Added stack navigation and tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './src/components/login';
+import Home from './src/components/home';
 
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 export default function App() {
+
+  const MainTabNavigator = () => (
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarActiveTintColor: '#145a32',
+      tabBarInactiveTintColor: 'gray',
+      headerTitleAlign: 'center',
+    })}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+        name='LoginScreen'
+        component={ Login }
+        options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="MainTabs"
+          component={MainTabNavigator}
+          options={{ headerShown: false, gestureEnabled: false }}
+        />
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/login/index.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "react-native";
+
+export default function Login(){
+    return(
+        <View style= {{backgroundColor : 'green', flex : 1}}>
+            <Text>Login</Text>
+        </View>
+    )
+}
